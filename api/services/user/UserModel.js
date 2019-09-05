bc9436b398a32c9eaa133ae9256a2e809bb06858
mvallarino <matias.vallarino@gmail.com>
2019-09-05 14:48:03 -0500

    Agrega validacion unique en MongoDB

--- api/services/user/UserModel.js
+++ api/services/user/UserModel.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  email: { type: String, required: true },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true
+    // match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+  },
   password: { type: String, required: true },
   deleted: { type: Boolean, default: false }
 });
